ca4e155f8bb96677e3bf2ba3a64bb5426bc32900
Rhiana Rutherford <19rutherford@stfrancishs.org>
2018-10-01 19:37:26 +0000

    updates

--- 42while24.c
+++ 42while24.c
@@ -4,19 +4,24 @@ int main(void)
     int sum=0;
     int k;
 
-    for (k=0;k<5;k++)
+    k=10;
+    while(k<5)
+    //for (k=0;k<5;k++)
     {
-        sum+=k; //use something different in "while" version
+        sum+=k;
+        k++;
     }
 
     printf ("%d\n",sum);
 
-    k=5;
+    // k=0;
 
-    while (k>0) {
-        k=k-1; //use something different in "for" version
+    for(k=10;k>=0;k--) {
+    //while (k>0){
+        //k=k-1;
         printf ("%d\n", k);
+
     }
+    printf ("BLAST OFF!");
 
 }
-
